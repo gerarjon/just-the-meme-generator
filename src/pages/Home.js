c16dd6c5ed03561ee81563df22f68efdbaaba6a1
gerarjon <gerarsuaverdez@gmail.com>
2021-04-14 14:01:40 -0700

    add home page backbone and bulmba css

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from '../components/container'
+
+// app
+function Home(props) {
+  return(
+    <Container>
+      <div className="columns">
+        <div className="column">
+          hello
+        </div>
+        <div className="column">
+          hello2
+        </div>
+        <div className="column">
+          hello3
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export default Home;
